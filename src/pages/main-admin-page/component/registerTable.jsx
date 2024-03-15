a5843ab503fe2d188cb1bda4c7d0dc0eac289bb6
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-03-15 10:19:45 +0300

    sasas

--- src/pages/main-admin-page/component/registerTable.jsx
+++ src/pages/main-admin-page/component/registerTable.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const RegisterTable = () => {
     return (
-        <div className="flex  flex-col  ">
-            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 sm:px-6 lg:px-8">
+        <div className="flex  flex-col ml-[7.5rem] ">
+            <div className="overflow-x-auto  sm:-mx-6 lg:-mx-8">
+                <div className="py-2 sm:px-6 lg:px-8 ">
                     <div className="overflow-hidden">
                         <table
-                            className="min-w-[90%]  border border-[#F46A06] text-center text-sm font-light text-surface  dark:text-white">
+                            className="min-w-[90%]  border-[2px] border-[#F46A06] rounded text-center text-sm font-light">
                             <thead
                                 className="border-b border-neutral-200 font-medium ">
                                 <tr>
@@ -43,9 +43,9 @@ const RegisterTable = () => {
                                         className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 text-start ">
                                         Otto
                                     </td>
-                                    <td className="whitespace-nowrap px-6 py-4 text-start">Hotel Des milles collines
-                                        <a href="#">Edit</a>
-                                        <a href="#" className='pl-5'>Delete</a>
+                                    <td className="whitespace-nowrap px-2 py-4 text-start">Hotel Des milles collines
+                                        <a href="#" className='px-4 py-2 rounded-lg text-white ml-4 mr-4 bg-[#F46A06]'>Edit</a>
+                                        <a href="#" className='px-4 py-2 rounded-lg text-white bg-[#F46A06]'>Delete</a>
                                     </td>
                                 </tr>
                             </tbody>
